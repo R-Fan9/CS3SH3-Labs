6bde4f38be420544133bac3530602ceb944533a9
Ricky Fan <rickyhzfan@gmail.com>
2022-09-28 22:48:22 -0400

    Revert "make clean on jiffies"
    This reverts commit 185f2b68d6ca911b5da29afa68a96432cc530ff9.

---
+++ lab1/jiffies/.jiffies.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/osc/labs/lab1/jiffies/jiffies.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/osc/labs/lab1/jiffies/jiffies.ko /home/osc/labs/lab1/jiffies/jiffies.o /home/osc/labs/lab1/jiffies/jiffies.mod.o
---
+++ lab1/jiffies/.tmp_versions/jiffies.mod
@@ -0,0 +1,2 @@
+/home/osc/labs/lab1/jiffies/jiffies.ko
+/home/osc/labs/lab1/jiffies/jiffies.o
