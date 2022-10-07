80bd2ce3285bbd94862f560a730e7edc4c6c4953
Ricky Fan <rickyhzfan@gmail.com>
2022-10-06 23:34:25 -0400

    used textbook impl for q2

--- lab2/q2/producer.c
+++ lab2/q2/producer.c
@@ -6,9 +6,6 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
-#include <unistd.h>
-#include <sys/types.h>
-
 int main()
 {
     /* the size (in bytes) of shared memory object */
@@ -16,26 +13,22 @@ int main()
     /* name of the shared memory object */
     const char *name = "OS";
     /* strings written to shared memory */
-    const char *message_0 = "Lab 2";
-    const char *message_1 = "Practice!";
+    const char *message_0 = "Hello";
+    const char *message_1 = "World!";
     /* shared memory file descriptor */
     int fd;
     /* pointer to shared memory obect */
     char *ptr;
-
     /* create the shared memory object */
     fd = shm_open(name, O_CREAT | O_RDWR, 0666);
     /* configure the size of the shared memory object */
     ftruncate(fd, SIZE);
     /* memory map the shared memory object */
-    ptr = (char *)
-        mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
+    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     /* write to the shared memory object */
-    sprintf(ptr, "%s", message_0);
-    ptr += strlen(message_0);
-    sprintf(ptr, "%s", message_1);
-    ptr += strlen(message_1);
-    getchar();
+    sprintf(ptr, "%s", message 0);
+    ptr += strlen(message 0);
+    sprintf(ptr, "%s", message 1);
+    ptr += strlen(message 1);
     return 0;
 }
