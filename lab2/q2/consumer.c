3a026ad05883fd1a5dc76ebef3bda14ba5a3b60c
Ricky Fan <rickyhzfan@gmail.com>
2022-10-18 18:15:57 -0600

    corrected q2

--- lab2/q2/consumer.c
+++ lab2/q2/consumer.c
@@ -1,5 +1,21 @@
+/**
+ * Simple program demonstrating shared memory in POSIX systems.
+ *
+ * This is the consumer process
+ *
+ * Figure 3.18
+ *
+ * To compile, enter
+ *	gcc shm-posix-consumer.c -lrt
+ *
+ * @author Gagne, Galvin, Silberschatz
+ * Operating System Concepts - Tenth Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
@@ -7,21 +23,27 @@
 
 int main()
 {
-    /* the size (in bytes) of shared memory object */
-    const int SIZE = 4096;
-    /* name of the shared memory object */
-    const char *name = "OS";
-    /* shared memory file descriptor */
-    int fd;
-    /* pointer to shared memory obect */
-    char *ptr;
-    /* open the shared memory object */
-    fd = shm_open(name, O_RDONLY, 0666);
-    /* memory map the shared memory object */
-    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    /* read from the shared memory object */
-    printf("%s", (char *)ptr);
-    /* remove the shared memory object */
-    shm_unlink(name);
-    return 0;
+	const char *name = "OS";
+	const int SIZE = 4096;
+
+	int shm_fd;
+	void *ptr;
+	int i;
+
+	/* open the shared memory segment */
+	shm_fd = shm_open(name, O_RDONLY, 0666);
+
+	/* now map the shared memory segment in the address space of the process */
+	ptr = mmap(0,SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+
+	/* now read from the shared memory region */
+	printf("%s",(char *)ptr);
+
+	/* remove the shared memory segment */
+	if (shm_unlink(name) == -1) {
+		printf("Error removing %s\n",name);
+		exit(-1);
+	}
+
+	return 0;
 }
