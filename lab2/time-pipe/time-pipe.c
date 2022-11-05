d2b1d2c4d2c651903bd5d0d02b56629ac6e44286
Ricky Fan <rickyhzfan@gmail.com>
2022-11-04 22:07:48 -0400

    added lab2 report

--- lab2/time-pipe/time-pipe.c
+++ lab2/time-pipe/time-pipe.c
@@ -1,9 +1,9 @@
 /**
  * time-pipe.c
  *
- * C program that calculates 
- * the execution time of commands  
- * using pipe 
+ * C program that calculates
+ * the execution time of commands
+ * using pipe
  *
  * fanh11@mcmaster.ca
  */
@@ -14,33 +14,33 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 
-// define the index for the read end of the pipe 
+// define the index for the read end of the pipe
 #define READ_END 0
-// define the index for the write end of the pipe 
+// define the index for the write end of the pipe
 #define WRITE_END 1
 
 int main(int argc, char *argv[])
 {
     /**
-     * define an array to store the file descriptor 
+     * define an array to store the file descriptors
      * for the read & write end of the pipe
      */
     int fd[2];
     // process Id
     pid_t pid;
-    // variable to keep track of the current time 
+    // variable to keep track of the current time
     struct timeval current_time;
 
     struct timeval *ptr = &current_time;
-    
-	/* create the pipe */
+
+    /* create the pipe */
     if (pipe(fd) == -1)
     {
         fprintf(stderr, "Pipe failed");
         return -1;
     }
 
-	/* fork a child process */
+    /* fork a child process */
     pid = fork();
 
     if (pid < 0)
@@ -53,14 +53,14 @@ int main(int argc, char *argv[])
     {
         /* close the read end of the pipe when write is about to happen*/
         close(fd[READ_END]);
-        
-	    /* get a record of the current time before executing the input command */
+
+        /* get a record of the current time before executing the input command */
         gettimeofday(&current_time, NULL);
 
-	    /* write the address of the current time to the pipe */
+        /* write the address of the current time to the pipe */
         write(fd[WRITE_END], ptr, sizeof(struct timeval));
 
-	    /* close the write end of the pipe*/
+        /* close the write end of the pipe*/
         close(fd[WRITE_END]);
 
         execvp(argv[1], &argv[1]);
@@ -74,22 +74,22 @@ int main(int argc, char *argv[])
         /* close the write end of the pipe when read is about to happen*/
         close(fd[WRITE_END]);
 
-	    /* read the address of the start time record from the pipe */
+        /* read the address of the start time record from the pipe */
         read(fd[READ_END], ptr, sizeof(struct timeval));
-        
-	    /* calculate the start time by converting to seconds */
-        double st_sec = ptr->tv_sec+ptr->tv_usec/1000000.0;
 
-	    /* get the current time after the input command has completed in the child process*/
+        /* calculate the start time by converting to seconds */
+        double st_sec = ptr->tv_sec + ptr->tv_usec / 1000000.0;
+
+        /* get the current time after the input command has completed in the child process*/
         gettimeofday(&current_time, NULL);
 
-	    /* calculate the current time by converting to seconds */
-        double ct_sec = current_time.tv_sec+current_time.tv_usec/1000000.0;
+        /* calculate the current time by converting to seconds */
+        double ct_sec = current_time.tv_sec + current_time.tv_usec / 1000000.0;
 
-	    /* display the elasped time for executing the input command */
+        /* display the elasped time for executing the input command */
         printf("\nElapsed time: %lf seconds\n", ct_sec - st_sec);
 
-	    /* close the read end of the pipe*/
+        /* close the read end of the pipe*/
         close(fd[READ_END]);
     }
 
--- lab2/time-shm/time-shm.c
+++ lab2/time-shm/time-shm.c
@@ -1,9 +1,9 @@
 /**
  * time-shm.c
  *
- * C program that calculates 
- * the execution time of commands  
- * using share memory object 
+ * C program that calculates
+ * the execution time of commands
+ * using share memory object
  *
  * fanh11@mcmaster.ca
  */
@@ -30,19 +30,19 @@ int main(int argc, char *argv[])
     void *ptr;
     // process id
     pid_t pid;
-    // variable to keep track of the current time 
+    // variable to keep track of the current time
     struct timeval current_time;
 
-	/* create the shared memory segment */
+    /* create the shared memory segment */
     fd = shm_open(name, O_CREAT | O_RDWR, 0666);
 
-	/* configure the size of the shared memory segment */
+    /* configure the size of the shared memory segment */
     ftruncate(fd, SIZE);
 
-	/* map the shared memory segment in the address space of the process */
+    /* map the shared memory segment in the address space of the process */
     ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-	/* fork a child process */
+    /* fork a child process */
     pid = fork();
 
     if (pid < 0)
@@ -53,30 +53,31 @@ int main(int argc, char *argv[])
     /* child process */
     else if (pid == 0)
     {
-	    /* get a record of the current time before executing the input command */
+        /* get a record of the current time before executing the input command */
         gettimeofday(&current_time, NULL);
 
         /**
          * calculate the current time by converting it into seconds
          * then write the result to the shared memory region
          */
-        sprintf(ptr, "%lf", current_time.tv_sec + current_time.tv_usec/1000000.0);
+        sprintf(ptr, "%lf", current_time.tv_sec + current_time.tv_usec / 1000000.0);
 
-	    /* execute the input command */
+        /* execute the input command */
         execvp(argv[1], &argv[1]);
     }
     /* parent process */
     else
     {
-	    /* wait for the child process to complete */
+        /* wait for the child process to complete */
         wait(NULL);
-	    /* get the current time after the input command has completed in the child process*/
+
+        /* get the current time after the input command has completed in the child process*/
         gettimeofday(&current_time, NULL);
 
-	    /* open the shared memory segment */
+        /* open the shared memory segment */
         fd = shm_open(name, O_RDONLY, 0666);
-        
-	    /* read from the shared memory region */
+
+        /* read from the shared memory region */
         ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, fd, 0);
         if (ptr == MAP_FAILED)
         {
@@ -85,15 +86,16 @@ int main(int argc, char *argv[])
         }
 
         char *rmn;
-	    /* convert the start time record from the chlld process to numeric value */
-        double st_sec = strtod((char *) ptr, &rmn);
-	    /* calculate the current time by converting it into seconds */
-        double ct_sec = current_time.tv_sec+current_time.tv_usec/1000000.0;
+        /* convert the start time record from the chlld process to numeric value */
+        double st_sec = strtod((char *)ptr, &rmn);
 
-	    /* display the elasped time for executing the input command */
+        /* calculate the current time by converting it into seconds */
+        double ct_sec = current_time.tv_sec + current_time.tv_usec / 1000000.0;
+
+        /* display the elasped time for executing the input command */
         printf("\nElasped time: %lf seconds\n", ct_sec - st_sec);
 
-	    /* remove the shared memory segment */
+        /* remove the shared memory segment */
         shm_unlink(name);
     }
 
